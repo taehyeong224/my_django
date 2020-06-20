559dc8b71fec6222631cef7243aa45e0cb27926c
kimtaehyeong <taehyeong224@gmail.com>
2020-06-21 00:24:55 +0900

    setting

--- my_django/settings.py
+++ my_django/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import pymysql
-
+pymysql.version_info = (1, 3, 13, "final", 0)
 pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
